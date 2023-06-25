5793d43012b919d16601bb5c12f670ce3174715a
Stefany María Méndez Salas <128415958+Tefis15@users.noreply.github.com>
2023-06-25 06:34:21 +0000

    Primera Version

--- src/app.js
+++ src/app.js
@@ -7,5 +7,64 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let intervaloNewCard;
+  let intervaloClearCard;
+  newCard();
+
+  intervaloClearCard = setInterval(clearCard, 10000);
+  intervaloNewCard = setInterval(newCard, 10000);
+
+  let button = document.getElementById("btnNewCard");
+  button.addEventListener("click", event => {
+    clearCard();
+    newCard();
+    clearInterval(intervaloClearCard);
+    clearInterval(intervaloNewCard);
+    intervaloClearCard = null;
+    intervaloNewCard = null;
+
+    intervaloClearCard = setInterval(clearCard, 10000);
+    intervaloNewCard = setInterval(newCard, 10000);
+  });
 };
+
+function newCard() {
+  document.querySelector(".card").classList.add(generateRandomSuit());
+  document.querySelector(".card").innerHTML =
+    "<span>" + generateRandomNumber() + "</span>";
+}
+
+function clearCard() {
+  document
+    .querySelector(".card")
+    .classList.remove("heart", "spade", "diamond", "club");
+  document.querySelector(".card").innerHTML = "<span></span>";
+}
+
+function generateRandomNumber() {
+  let numbers = [
+    "A",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let index = Math.floor(Math.random() * 12);
+
+  return numbers[index];
+}
+function generateRandomSuit() {
+  let suit = ["heart", "spade", "club", "diamond"];
+  let index = Math.floor(Math.random() * 3);
+
+  return suit[index];
+}
